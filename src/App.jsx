cb468e3b4b80f14829bed249bf2217b7a629c7bb
unknown <you@example.com>
2024-07-11 15:44:54 +0000

    update sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Landing from "./pages/landing";
-import SignUp from "./pages/authentification";
 import Preview from "./pages/preview";
 import Dashboard from "./pages/dashboard";
+import Signup from "./pages/authentification/signup";
+import Signin from "./pages/authentification/signin";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/signup", element: <SignUp /> },
+    { path: "/signup", element: <Signup/> },
     { path: "/preview", element: <Preview /> },
     { path: "/dashboard", element: <Dashboard /> },
     { path: "/landing", element: <Landing /> },
+    {path:  "/signin", element:<Signin/>}
   ]);
 
   return <RouterProvider router={router} />;

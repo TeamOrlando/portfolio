d8091e58760e254fa64912225339eb10e52e5eb3
Elorm Wuaku <hageelorm@gmail.com>
2024-07-15 09:16:57 +0000

    Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     { path: "/signup", element: <Signup/> },
     { path: "/preview", element: <Preview /> },
     { path: "/dashboard", element: <Dashboard /> },
-    { path: "/", element: <Landing /> },
+    { path: "/landing", element: <Landing /> },
     {path:  "/signin", element:<Signin/>}
   ]);
 

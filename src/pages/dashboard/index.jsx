aa0d1f453f3e8b9042a6978683defc32d91e6232
OmniLorie <Losibe27@gmail.com>
2024-07-17 16:42:16 -0400

    integration dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,4 +1,41 @@
+import { useEffect, useState } from "react";
+import { apiGetExperiences } from "../../services/experience";
+import { apiGetSkills } from "../../services/skills";
+
 const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState();
+  const getData = async () => {
+    setIsLoading(true);
+    try {
+      const [
+        totalSkills,
+        totalExperiences,
+        // totalProjects,
+      ] = await Promise.all([
+        apiGetSkills,
+        apiGetExperiences,
+        // apiGetProjects,
+      ]);
+
+      const newData = {
+        skills: totalSkills.length,
+        // projects: totalProjects.length,
+        experiences: totalExperiences.length,
+        // socials: totalSocials.length,
+      };
+      console.log("data", newData);
+      setData(newData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    //getData();
+  });
   return <div>Dashboard</div>;
 };
 

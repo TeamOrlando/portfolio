722d47a2a728452d5f89d81fb7a3015f909d8bff
Elorm Wuaku <hageelorm@gmail.com>
2024-07-17 13:27:15 +0000

    Final changes to page loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,3 @@
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Landing from "./pages/landing";
-// import Preview from "./pages/preview";
-// import Dashboard from "./pages/dashboard";
-// import Signup from "./pages/authentification/signup";
-// import Signin from "./pages/authentification/signin";
-
-// function App() {
-//   const router = createBrowserRouter([
-//     { path: "/signup", element: <Signup/> },
-//     { path: "/preview", element: <Preview /> },
-//     { path: "/dashboard", element: <Dashboard /> },
-//     { path: "/landing", element: <Landing /> },
-//     {path:  "/signin", element:<Signin/>}
-//   ]);
-
-//   return <RouterProvider router={router} />;
-// }
-// export default App;
 
 import { useState, useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";

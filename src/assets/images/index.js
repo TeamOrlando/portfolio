c421fd77c4ef4db46374238416ffd85e0ac26a7c
elikandid <edogbey64@gmail.com>
2024-07-18 14:14:47 +0000

    update landing page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,7 @@ import fashionBlog from "../images/fashionBlog.jpg"
 import herobg from "../images/herobg.jpg"
 import kitchen from "../images/kitchen.jpg"
 import weather from "../images/weather.jpg"
+import logo from "../images/logo.png"
+import landingBg from "../images/landingBg.jpg"
 
-export  {eye, fashionBlog, herobg, kitchen, weather }
+export  {eye, fashionBlog, herobg, kitchen, weather,logo, landingBg}
